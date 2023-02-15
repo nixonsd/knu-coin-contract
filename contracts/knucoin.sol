bcfa513b258b43ce332ee48aa423ca3e638fb95d
Bohdan Kiselov <bandanovvs@gmail.com>
2023-02-16 01:08:34 +0200

    upadate contract

--- contracts/knucoin.sol
+++ contracts/knucoin.sol
@@ -1,57 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "./ierc20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract KNUCoin is IERC20 {
-    uint public totalSupply;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
-    string public name = "KNU Coin";
-    string public symbol = "KNUCOIN";
+contract KNUCoin is ERC20 {
     address payable public owner;
-    uint8 public decimals = 18;
 
-    constructor() payable {
+    constructor(uint256 initialSupply) ERC20('KNU Coin Token', "KNUCOIN") {
         owner = payable(msg.sender);
-    }
-
-    function transfer(address recipient, uint amount) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function approve(address spender, uint amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
+        _mint(msg.sender, initialSupply);
     }
 
     function mint(uint amount) external {
         require(msg.sender == owner, "You aren't the owner");
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
-        emit Transfer(address(0), msg.sender, amount);
+        _mint(msg.sender, amount);
     }
 
     function burn(uint amount) external {
         require(msg.sender == owner, "You aren't the owner");
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Transfer(msg.sender, address(0), amount);
+        _burn(msg.sender, amount);
     }
 }
--- test/knucoin.ts
+++ test/knucoin.ts
@@ -2,68 +2,59 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
 import { KNUCoin } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe('KNUCoin ERC20 Contract', () => {
   let knuCoin: KNUCoin;
+  let initialAmount = ethers.BigNumber.from(100000);
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+	let addr2: SignerWithAddress
+	let addrs: SignerWithAddress[];
   
-  async function deployCoin() {
-    const ONE_GWEI = 1_000_000_000;
-    const lockedAmount = ONE_GWEI;
-    const [ owner, otherAccount ] = await ethers.getSigners();
+  beforeEach(async () => {
+    [ owner, addr1, addr2, ...addrs ] = await ethers.getSigners();
     const KNUCoinFactory = await ethers.getContractFactory("KNUCoin");
-    knuCoin = await KNUCoinFactory.deploy({value: lockedAmount});
-    return { knuCoin, owner, otherAccount };
-  }
+    knuCoin = await KNUCoinFactory.deploy(initialAmount);
+  });
 
   describe('Deployment', () => {
     it('should set the right owner', async () => {
-      const { knuCoin, owner } = await loadFixture(deployCoin);
       expect(await knuCoin.owner()).to.equal(owner.address);
+    });
+
+    it('should set initial balance', async () => {
+      expect(await knuCoin.totalSupply()).to.equal(initialAmount);
     });
   });
 
   describe('Mint and burn ownership', () => {
     it('should complete mint', async () => {
       const amount = ethers.BigNumber.from(100);
-      const { knuCoin, owner } = await loadFixture(deployCoin);
       await knuCoin.mint(amount);
-      expect(await knuCoin.balanceOf(owner.address)).to.equal(amount);
+      expect(await knuCoin.balanceOf(owner.address)).to.equal(initialAmount.add(amount));
     });
 
     it('should revert mint', async () => {
       const amount = ethers.BigNumber.from(100);
-      const { knuCoin, otherAccount } = await loadFixture(deployCoin);
-      await expect(knuCoin.connect(otherAccount).mint(amount)).to.be.revertedWith('You aren\'t the owner');
+      await expect(knuCoin.connect(addr1).mint(amount)).to.be.revertedWith('You aren\'t the owner');
     });
 
     it('should complete burn', async () => {
       const amount = ethers.BigNumber.from(100);
-      const { knuCoin, owner } = await loadFixture(deployCoin);
       await knuCoin.mint(amount);      
       await knuCoin.burn(amount);
-      expect(await knuCoin.balanceOf(owner.address)).to.equal(0);
+      expect(await knuCoin.balanceOf(owner.address)).to.equal(initialAmount);
     });
 
     it('should revert burn', async () => {
       const amount = ethers.BigNumber.from(100);
-      const { knuCoin, otherAccount } = await loadFixture(deployCoin);
-      await expect(knuCoin.connect(otherAccount).burn(amount)).to.be.revertedWith('You aren\'t the owner');
+      await expect(knuCoin.connect(addr1).burn(amount)).to.be.revertedWith('You aren\'t the owner');
     });
   });
 
-  describe('Trasnfers', () => {
-    it('should mint throw transfer event', async () => {
-      const amount = ethers.BigNumber.from(100);
-      const { knuCoin } = await loadFixture(deployCoin);
-      await expect(knuCoin.mint(amount)).to.emit(knuCoin, 'Transfer');
-    });
-
-    it('should burn throw transfer event', async () => {
-      const amount = ethers.BigNumber.from(100);
-      const { knuCoin } = await loadFixture(deployCoin);
-      knuCoin.mint(amount);
-      await expect(knuCoin.burn(amount)).to.emit(knuCoin, 'Transfer');
-    });
+  describe('Transactions', () => {
+    
   });
 })
