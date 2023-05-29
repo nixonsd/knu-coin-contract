556b8abd59cef1169d5acfedc4801d19cebb78cf
Bohdan Kiselov <bandanovvs@gmail.com>
2023-05-29 14:22:04 +0300

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {},
+    polygon: {
+      url: 'https://polygon-rpc.comm',
+      accounts: [ process.env.PRIVATE_KEY as string ],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY as string,
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,16 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function main() {
-  const KNUCoin = await ethers.getContractFactory("KNUCOIN");
-  const coin = await KNUCoin.deploy(475393530, 4, "KNUCOIN", "KNUCOIN");
+  const KNUCoin = await ethers.getContractFactory('KNUCOIN');
+  const coin = await KNUCoin.deploy(
+    Number(process.env.TEACHER_TELEGRAM_ID as string), 
+    Number(process.env.TEACHER_ARRANGEMENT_LIMIT as string), 
+    process.env.CONTRACT_NAME as string, 
+    process.env.CONTRACT_SYMBOL as string,
+    );
 
   await coin.deployed();
 
